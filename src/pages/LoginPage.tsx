88511c2062245407aeeb5e71aad809d6e9da242e
CristiSandu <crystisandu3@gmail.com>
2022-04-17 23:38:12 +0300

    Changes:  - add colors  - add login page  - add google icon

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,19 +1,49 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import GoogleLogo from "../Images/gLogo.svg";
+
 export const LoginPage = (): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className="text-3xl font-bold">
-          Edit sasas <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex h-screen">
+      <div className="m-auto space-y-64 text-center ">
+        <div className="space-y-8">
+          <form className="space-y-4 align-baseline ">
+            <div>
+              <i className="fa fa-instagram icon"></i>
+              <input
+                className="entry-primary"
+                type="text"
+                name="name"
+                placeholder="Email"
+              />
+            </div>
+            <div>
+              <input
+                className="entry-primary"
+                type="password"
+                name="name"
+                placeholder="Password"
+              />
+            </div>
+          </form>
+          <div className="space-x-10">
+            <button className="btn-primary">Login</button>
+            <button className="btn-primary">Register</button>
+          </div>
+          <div className="flex justify-center items-center space-x-2">
+            <div className="bg-black h-0.5 w-full content-center"></div>
+            <div className="font-semibold">OR</div>
+            <div className="bg-black h-0.5 w-full "></div>
+          </div>
+          <div
+            className="flex items-center space-x-2 justify-center btn-primary cursor-pointer"
+            onClick={() => {}}
+          >
+            <img src={GoogleLogo} alt="React Logo" />
+            <h1>Login with Google</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        MainBlue: "#0077B5",
+        SecondBlue: "#12ACFF",
+        LightBlue: "#A0DEFF",
+        WhiteBlue: "#E7F7FF",
+        White: "#FFFFFF",
+        Black: "#000000",
+
+        RedDelete: "#B50000",
+        GreenCheck: "#19B500",
+        YellowWaiting: "#E7DF24",
+      },
+    },
   },
   plugins: [],
 };
